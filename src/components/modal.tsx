d4b3e0740c7dfd25810625077bec448778d48e29
Thalysson Leite <ThalyssonLeite@users.noreply.github.com>
2025-11-25 17:46:35 -0300

    feat: make modal thiner

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -7,7 +7,7 @@ type Props = {
 export function Modal({ children }: Props) {
 	return (
 		<div className="fixed inset-0 flex items-center justify-center">
-			<div className="flex rounded-xl overflow-hidden text-white bg-black w-[clamp(24rem,80vw,90vw)] h-[clamp(20rem,80vh,90vh)]">
+			<div className="flex rounded-xl overflow-hidden text-white bg-black w-[clamp(24rem,65vw,90vw)] h-[clamp(20rem,80vh,90vh)]">
 				{children}
 			</div>
 		</div>
